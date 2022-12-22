57362aa4a190b1b69fb3b8b44602800b14e183e6
Vadim <vingvadim@yandex.ru>
2022-12-22 08:31:37 +0300

    optimized algorithm with less iterations

--- numpy/practical tasks/10.8.py
+++ numpy/practical tasks/10.8.py
@@ -6,12 +6,12 @@ def min_max_dist(*vectors):
     min_dist = None
     max_dist = None
     
-    # Check each vector with each
-    for vector1 in vectors:
+    # Check each vector with each but without repeats
+    for index_1, vector1 in enumerate(vectors):
         
-        for vector2 in vectors:
+        for index_2 in range(index_1 + 1, len(vectors)):
 
-            check_dist = np.linalg.norm(vector1 - vector2)
+            check_dist = np.linalg.norm(vector1 - vectors[index_2])
                 
             if min_dist is None and max_dist is None:
                 
@@ -37,5 +37,8 @@ def min_max_dist(*vectors):
 vec1 = np.array([1,2,3])
 vec2 = np.array([4,5,6])
 vec3 = np.array([7, 8, 9])
+vec4 = np.array([9, 10, 11])
+vec5 = np.array([0.3, 0.5, 0.9])
+
  
-print(min_max_dist(vec1, vec2, vec3))
+print(min_max_dist(vec1, vec2, vec3, vec4, vec5))
